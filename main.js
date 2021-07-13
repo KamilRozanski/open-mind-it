9bbe4d0842f79870eedfe09e1d40401fcc456a00
Kamil Rozanski <motomc1@gmail.com>
2021-07-13 11:35:01 +0100

    add hamburger menu

---
+++ main.js
@@ -0,0 +1,6 @@
+// const hamburgerMenu = document.querySelector(".nav__menu__hamburger__inner")
+
+// hamburgerMenu.addEventListener("click", function () {
+//     hamburgerMenu.classList.toggle("nav__menu__hamburger--active")
+//     console.log("click")
+// })
