e8ccb162c9f4c1bc1f986f697d277a3a3357b874
Kamil Rozanski <motomc1@gmail.com>
2021-07-18 14:07:41 +0100

    drop manu must be added

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const hamburgerMenu = document.querySelector(".nav__menu__hamburger")
+const navMenu = document.querySelector(".nav__menu")
 
 hamburgerMenu.addEventListener("click", function () {
     hamburgerMenu.classList.toggle("hamburger--active")
+    navMenu.classList.toggle("navMenu--active")
 
     console.log("click")
 })
