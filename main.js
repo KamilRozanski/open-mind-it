5db3d7416d6891aa6c54a0139218b90247dcbcc5
KamilRozanski <motomc1@gmail.com>
2021-08-16 12:50:23 +0100

    RWD done

--- main.js
+++ main.js
@@ -9,14 +9,6 @@ hamburgerMenu.addEventListener("click", function () {
     console.log("click")
 })
 
-function fixedMenu() {
-    let currentlyScrolledPixel = window.scrollY
-    let = hamburgerMenuHeight = document.querySelector(".nav__menu__hamburger").offsetTop;
-
-    currentlyScrolledPixel >= hamburgerMenuHeight ? hamburgerMenu.classList.add("nav__menu__hamburger--sticy") : hamburgerMenu.classList.remove("nav__menu__hamburger--sticy")
-}
-
-window.addEventListener("scroll", fixedMenu)
 
 
 // show/hide phone/email
