2af259a5558d53825c9de4b8857117731e70594a
KamilRozanski <motomc1@gmail.com>
2021-09-13 11:33:44 +0100

    new hamburger menu animation

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-
 let hamburgerMenu = document.querySelector(".nav__menu__hamburger")
-const navMenu = document.querySelector(".nav__menu ")
+const navMenu = document.querySelector(".nav__menu")
 
 
 hamburgerMenu.addEventListener("click", function () {
     hamburgerMenu.classList.toggle("hamburger--active")
     navMenu.classList.toggle("navMenu--active")
-
-    if (navMenu.classList.contains("navMenu--active")) {
-        navMenu.classList.remove("navMenu--no-active")
-    } else {
-        navMenu.classList.add("navMenu--no-active")
-    }
 })
 
 
